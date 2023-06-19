ffe91a84582b5f970559126254a674855493debe
Dariia Paliy <dariia.palij@gmail.com>
2023-06-19 13:15:13 +0300

    added section - testimonials +resposive && jquery plugin - owl-carousel

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,35 @@ burger.addEventListener("click", function () {
     this.classList.toggle("active");
     headerMenu.classList.toggle("open");
 });
+
+// ----------------------- Carousel-----------------------
+
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+        },
+        600: {
+            items: 2,
+            nav: false,
+        },
+        1000: {
+            items: 3,
+            nav: false,
+        },
+    },
+});
+$(".play").on("click", function () {
+    owl.trigger("play.owl.autoplay", [2000]);
+});
+$(".stop").on("click", function () {
+    owl.trigger("stop.owl.autoplay");
+});
